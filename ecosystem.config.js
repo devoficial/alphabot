7c6d374140427aa138a78513e546595e4892db83
devofficial <debasisnath85@gmail.com>
2018-08-30 17:09:09 +0530

    added ip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,14 @@ module.exports = {
             script: 'bin/run.js',
             env_production: {
                 NODE_ENV: 'production',
-                alphaUrl: 'http://172.31.30.91:3010'
+                alphaUrl: 'http://18.188.169.23:3010'
             }
         }
     ],
     deploy: {
         production: {
             user: 'dev',
-            host: '172.31.30.91',
+            host: '18.188.169.23',
             ref: 'new/master',
             repo: 'https://github.com/devoficial/alphabot.git',
             path: '/srv/production',
